e3a13eb28623cf9eb15a07071c5393ff752fe402
L. T. S <leevi.syvanen@hotmail.fi>
2017-08-28 14:16:52 +0300

    "Kill your darlings." Changed almost everything to make the app run smoother.

--- ASCII Converter.cpp
+++ ASCII Converter.cpp
@@ -6,11 +6,8 @@
 #include <chrono>
 
 
-HANDLE help = GetStdHandle(STD_OUTPUT_HANDLE);
-HANDLE decoding = CreateConsoleScreenBuffer(GENERIC_WRITE | GENERIC_READ, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-HANDLE info = CreateConsoleScreenBuffer(GENERIC_WRITE | GENERIC_READ, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-HANDLE encoding = CreateConsoleScreenBuffer(GENERIC_WRITE | GENERIC_READ, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-HANDLE currentHandle = help;
+HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+int tab = 1;
 
 DWORD written;
 CONSOLE_SCREEN_BUFFER_INFO screen;
@@ -28,63 +25,92 @@ void SetWindow(int width, int height) {
 	screen.srWindow.Bottom = height - 1;
 	screen.srWindow.Right = width - 1;
 
-	SetConsoleScreenBufferSize(currentHandle, coord);
-	SetConsoleWindowInfo(currentHandle, TRUE, &screen.srWindow);
+	SetConsoleScreenBufferSize(console, coord);
+	SetConsoleWindowInfo(console, TRUE, &screen.srWindow);
 }
 
 void clear() {
 	COORD topLeft = { 0, 0 };
 
-	GetConsoleScreenBufferInfo(currentHandle, &screen);
+	GetConsoleScreenBufferInfo(console, &screen);
 	FillConsoleOutputCharacterA(
-		currentHandle, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+		console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
 	);
 	FillConsoleOutputAttribute(
-		currentHandle, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+		console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
 		screen.dwSize.X * screen.dwSize.Y, topLeft, &written
 	);
-	SetConsoleCursorPosition(currentHandle, topLeft);
+
+	SetConsoleCursorPosition(console, topLeft);
 }
 
 void gotoxy(int x, int y) {
 	COORD coord;
 	coord.X = x;
 	coord.Y = y;
-	SetConsoleCursorPosition(currentHandle, coord);
+	SetConsoleCursorPosition(console, coord);
+}
+
+void waitForUpdate() {
+	CONSOLE_SCREEN_BUFFER_INFO screenOld = screen;
+	while (screenOld.srWindow.Right == screen.srWindow.Right && !GetAsyncKeyState(VK_RETURN) && !GetAsyncKeyState(VK_DOWN) && !GetAsyncKeyState(VK_UP) && !GetAsyncKeyState(VK_RIGHT) && !GetAsyncKeyState(VK_LEFT) && !GetAsyncKeyState(VK_ESCAPE)) {
+		GetConsoleScreenBufferInfo(console, &screen);
+		std::this_thread::sleep_for(std::chrono::milliseconds(5));
+	}
 }
 
 void draw() {
 	std::string space = "";
-	std::stringstream tab;
+	std::stringstream menu;
 
-	for (int i = 0; i < (screen.srWindow.Right - 28) / 5; i++) {
-		space.append(" ");
+	if (screen.srWindow.Right > 0) {
+		for (int i = 0; i < (screen.srWindow.Right - 28) / 5; i++) {
+			space.append(" ");
+		}
 	}
 
-	if (currentHandle == help) {
-		tab << space << ">Help<" << space << " Decode " << space << " Info " << space << " Encode\n";
-	} else if (currentHandle == decoding) {
-		tab << space << "Help" << space << ">Decode<" << space << " Info " << space << " Encode\n";
-	} else if (currentHandle == info) {
-		tab << space << "Help" << space << " Decode " << space << ">Info<" << space << " Encode\n";
-	} else {
-		tab << space << "Help" << space << " Decode " << space << " Info " << space << ">Encode<\n";
+	switch (tab) {
+	case 1:
+		menu << space << ">Help<" << space << " Decode " << space << " Info " << space << " Encode\n";
+		break;
+	case 2:
+		menu << space << "Help" << space << ">Decode<" << space << " Info " << space << " Encode\n";
+		break;
+	case 3:
+		menu << space << "Help" << space << " Decode " << space << ">Info<" << space << " Encode\n";
+		break;
+	case 4:
+		menu << space << "Help" << space << " Decode " << space << " Info " << space << ">Encode<\n";
+		break;
 	}
+
+	clear();
 
 	gotoxy(0, 0);
-	std::cout << tab.str();
+	std::cout << menu.str();
 
-	if (currentHandle == help) {
-		gotoxy(0, 3);
+	gotoxy(0, 2);
+	switch (tab) {
+	case 1:
 		std::cout << "Move selection with arrow keys.\nLeft and right to switch tabs.\nEnter / Return key to interact with tab.\nEscape key to exit programme.\n\nUp and down to change options in this tab.\n\n\nConversion:\n\n Decimals\n Hexadecimals\n Octals";
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
 	}
+
+	cursor.bVisible = false;
+	SetConsoleCursorInfo(console, &cursor);
 }
 
 void helptab() {
 	if (GetAsyncKeyState(VK_RETURN)) {
 		gotoxy(0, conversion);
 		cursor.bVisible = true;
-		SetConsoleCursorInfo(currentHandle, &cursor);
+		SetConsoleCursorInfo(console, &cursor);
 
 		while (GetAsyncKeyState(VK_RETURN)) {
 			std::this_thread::sleep_for(std::chrono::milliseconds(5));
@@ -104,50 +130,82 @@ void helptab() {
 			std::this_thread::sleep_for(std::chrono::milliseconds(5));
 		}
 
+		cursor.bVisible = false;
+		SetConsoleCursorInfo(console, &cursor);
+	} else if (GetAsyncKeyState(VK_RIGHT)) {
 		while (GetAsyncKeyState(VK_RETURN)) {
 			std::this_thread::sleep_for(std::chrono::milliseconds(5));
 		}
-
-		cursor.bVisible = false;
-		SetConsoleCursorInfo(currentHandle, &cursor);
-	} else if (GetAsyncKeyState(VK_RIGHT)) {
-		currentHandle = decoding;
-		SetConsoleActiveScreenBuffer(currentHandle);
+		tab = 2;
+		SetConsoleTitle(L"ASCII - Decode");
 	}
 }
 
 void decode() {
-
+	if (GetAsyncKeyState(VK_LEFT)) {
+		while (GetAsyncKeyState(VK_RETURN)) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(5));
+		}
+		tab = 1;
+		SetConsoleTitle(L"ASCII - Help");
+	} else if (GetAsyncKeyState(VK_RIGHT)) {
+		tab = 3;
+		SetConsoleTitle(L"ASCII - Info");
+	}
 }
 
 void infotab() {
-
+	if (GetAsyncKeyState(VK_LEFT)) {
+		tab = 2;
+		SetConsoleTitle(L"ASCII - Decode");
+	} else if (GetAsyncKeyState(VK_RIGHT)) {
+		tab = 4;
+		SetConsoleTitle(L"ASCII - Encode");
+	}
 }
 
 void encode() {
-
+	if (GetAsyncKeyState(VK_LEFT)) {
+		tab = 3;
+		SetConsoleTitle(L"ASCII - Info");
+	}
 }
 
 int main() {
 	SetConsoleTitle(L"ASCII - Help");
+
 	cursor.dwSize = 100;
-	cursor.bVisible = false;
-	SetConsoleCursorInfo(currentHandle, &cursor);
-	SetWindow(43, 16);
-	SetWindow(44, 17);
+
+	SetWindow(43, 15);
+	SetWindow(44, 16);
+
 	while (!GetAsyncKeyState(VK_ESCAPE)) {
+		switch (tab) {
+			case 1:
+				helptab();
+				break;
+			case 2:
+				decode();
+				break;
+			case 3:
+				infotab();
+				break;
+			case 4:
+				encode();
+				break;
+		}
+
 		draw();
 
-		if (currentHandle == decoding) {
-			decode();
-		} else if (currentHandle == info) {
-			infotab();
-		} else if (currentHandle == encoding) {
-			encode();
-		} else {
-			helptab();
+		while (GetAsyncKeyState(VK_RETURN) || GetAsyncKeyState(VK_LEFT) || GetAsyncKeyState(VK_RIGHT) || GetAsyncKeyState(VK_UP) || GetAsyncKeyState(VK_DOWN)) {
+			if (GetAsyncKeyState(VK_ESCAPE)) {
+				break;
+			}
+			std::this_thread::sleep_for(std::chrono::milliseconds(5));
 		}
-		std::this_thread::sleep_for(std::chrono::milliseconds(5));
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(25));
+		waitForUpdate();
 	}
     return 0;
 }
