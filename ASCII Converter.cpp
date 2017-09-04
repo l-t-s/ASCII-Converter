6edfe886b8a250e38c35fde2cf2c699761b77fa4
L. T. S <leevi.syvanen@hotmail.fi>
2017-09-04 13:08:12 +0300

    Trying desperately to make info better, while cursing at the thousands of string types.

--- ASCII Converter.cpp
+++ ASCII Converter.cpp
@@ -14,8 +14,8 @@ CONSOLE_CURSOR_INFO cursor;
 unsigned int tab = 1;
 unsigned int conversion = 12;
 unsigned int view = 0;
-unsigned int characters[102];
-std::string items[] = { "Length: ", "\n\nNumber of:\n    Spaces ( )                      ", "\n    Exclamations (!)                ", "\n    Quotations (\x22)                  ", "\n    Hashes(#)                       ", "\n    Dollars($)                      ", "\n    Percents(%)                     ", "\n    Ampersands(&)                   ", "\n    Apostrophes(')                  ", "\n    Opening Parentheses (()         ", "\n    Closing Parentheses ())         ", "\n    Asterisks (*)                   ", "\n    Plusses (+)                     ", "\n    Commas (,)                      ", "\n    Dashes (-)                      ", "\n    Periods (.)                     ", "\n    Slashes (/)                     ", "\n    Colons (:)                      ", "\n    Semi-Colons (;)                 ", "\n    Lesser Than Quillemets (<)      ", "\n    Equals (=)                      ", "\n    Greater Than Quillemets (>)     ", "\n    Questions (?)                   ", "\n    Ats (@)                         ", "\n    Opening Braces ([)              ", "\n    Backslashes (\\)                 ", "\n    Closing Braces (])              ", "\n    Carets (^)                      ", "\n    Underscore (_)                  ", "\n    Graves (`)                      ", "\n    Opening Curly Brackets ({)      ", "\n    Vertical Bars (|)               ", "\n    Closing Curly Brackets (})      ", "\n    Tildes (~)                      ", "\n    Pounds (£)                      ", "\n    Micros (µ)                      ", "\n    Obeluses (÷)                    ", "\n    Degrees (°)                     ", "\n    No-Breaking Spaces ( )          ", "\n    1                               ", "\n    2                               ", "\n    3                               ", "\n    4                               ", "\n    5                               ", "\n    6                               ", "\n    7                               ", "\n    8                               ", "\n    9                               ", "\n    0                               ", "\n    A                               ", "\n    B                               ", "\n    C                               ", "\n    D                               ", "\n    E                               ",  "\n    F                               ", "\n    G                               ", "\n    H                               ", "\n    I                               ", "\n    J                               ", "\n    K                               ", "\n    L                               ", "\n    M                               ", "\n    N                               ", "\n    O                               ", "\n    P                               ", "\n    Q                               ", "\n    R                               ", "\n    S                               ", "\n    T                               ", "\n    U                               ", "\n    V                               ", "\n    W                               ", "\n    X                               ", "\n    Y                               ", "\n    Z                               ", "\n    a                               ", "\n    b                               ", "\n    c                               ", "\n    d                               ", "\n    e                               ", "\n    f                               ", "\n    g                               ", "\n    h                               ", "\n    i                               ", "\n    j                               ", "\n    k                               ", "\n    l                               ", "\n    m                               ", "\n    n                               ", "\n    o                               ", "\n    p                               ", "\n    q                               ", "\n    r                               ", "\n    s                               ", "\n    t                               ", "\n    u                               " , "\n    v                               ", "\n    w                               ", "\n    x                               ", "\n    y                               ", "\n    z                               ", "\n\nUnrecognized characters:            " };
+unsigned int characters[105];
+LPCTSTR items[] = { L"Length: ", L"",  L"Number of:", L"    Spaces ( )                      ", L"    Exclamations (!)                ", L"    Quotations (\x22)                  ", L"    Hashes(#)                       ", L"    Dollars($)                      ", L"    Percents(%)                     ", L"    Ampersands(&)                   ", L"    Apostrophes(')                  ", L"    Opening Parentheses (()         ", L"    Closing Parentheses ())         ", L"    Asterisks (*)                   ", L"    Plusses (+)                     ", L"    Commas (,)                      ", L"    Dashes (-)                      ", L"    Periods (.)                     ", L"    Slashes (/)                     ", L"    Colons (:)                      ", L"    Semi-Colons (;)                 ", L"    Lesser Than Quillemets (<)      ", L"    Equals (=)                      ", L"    Greater Than Quillemets (>)     ", L"    Questions (?)                   ", L"    Ats (@)                         ", L"    Opening Braces ([)              ", L"    Backslashes (\\)                 ", L"    Closing Braces (])              ", L"    Carets (^)                      ", L"    Underscore (_)                  ", L"    Graves (`)                      ", L"    Opening Curly Brackets ({)      ", L"    Vertical Bars (|)               ", L"    Closing Curly Brackets (})      ", L"    Tildes (~)                      ", L"    Pounds (£)                      ", L"    Micros (µ)                      ", L"    Obeluses (÷)                    ", L"    Degrees (°)                     ", L"    No-Breaking Spaces ( )          ", L"    1                               ", L"    2                               ", L"    3                               ", L"    4                               ", L"    5                               ", L"    6                               ", L"    7                               ", L"    8                               ", L"    9                               ", L"    0                               ", L"    A                               ", L"    B                               ", L"    C                               ", L"    D                               ", L"    E                               ",  L"    F                               ", L"    G                               ", L"    H                               ", L"    I                               ", L"    J                               ", L"    K                               ", L"    L                               ", L"    M                               ", L"    N                               ", L"    O                               ", L"    P                               ", L"    Q                               ", L"    R                               ", L"    S                               ", L"    T                               ", L"    U                               ", L"    V                               ", L"    W                               ", L"    X                               ", L"    Y                               ", L"    Z                               ", L"    a                               ", L"    b                               ", L"    c                               ", L"    d                               ", L"    e                               ", L"    f                               ", L"    g                               ", L"    h                               ", L"    i                               ", L"    j                               ", L"    k                               ", L"    l                               ", L"    m                               ", L"    n                               ", L"    o                               ", L"    p                               ", L"    q                               ", L"    r                               ", L"    s                               ", L"    t                               ", L"    u                               " , L"    v                               ", L"    w                               ", L"    x                               ", L"    y                               ", L"    z                               ", L"", L"Unrecognized characters:            " };
 
 void SetWindow(int width, int height) {
 	_COORD coord;
@@ -27,8 +27,8 @@ void SetWindow(int width, int height) {
 	screen.srWindow.Bottom = height - 1;
 	screen.srWindow.Right = width - 1;
 
-	SetConsoleScreenBufferSize(console, coord);
 	SetConsoleWindowInfo(console, TRUE, &screen.srWindow);
+	SetConsoleScreenBufferSize(console, coord);
 }
 
 void clear() {
@@ -93,16 +93,32 @@ void draw() {
 	gotoxy(0, 2);
 	switch (tab) {
 	case 1:
-		std::cout << "Move selection with arrow keys.\nLeft and right to switch tabs.\nEnter / Return key to interact with tab.\nEscape key to exit programme.\n\nUp and down to change options in this tab.\n\n\nConversion:\n\n Decimals\n Hexadecimals\n Octals";
+		WriteConsoleOutputCharacter(console, L"Move selection with arrow keys.", 31, { 0, 2 }, &written);
+		WriteConsoleOutputCharacter(console, L"Left and right to switch tabs.", 30, { 0, 3 }, &written);
+		WriteConsoleOutputCharacter(console, L"Enter / Return key to interact with tab.", 40, { 0, 4 }, &written);
+		WriteConsoleOutputCharacter(console, L"Escape key to exit programme.", 29, { 0, 5 }, &written);
+		WriteConsoleOutputCharacter(console, L"Up and down to change options in this tab.", 42, { 0, 7 }, &written);
+		WriteConsoleOutputCharacter(console, L"Conversion:", 11, { 0, 10 }, &written);
+		WriteConsoleOutputCharacter(console, L"Decimals", 8, { 1, 12 }, &written);
+		WriteConsoleOutputCharacter(console, L"Hexadecimals", 12, { 1, 13 }, &written);
+		WriteConsoleOutputCharacter(console, L"Octals", 6, { 1, 14 }, &written);
+		//std::cout << "Move selection with arrow keys.\nLeft and right to switch tabs.\nEnter / Return key to interact with tab.\nEscape key to exit programme.\n\nUp and down to change options in this tab.\n\n\nConversion:\n\n Decimals\n Hexadecimals\n Octals";
 		break;
 	case 2:
 		break;
 	case 3:
+		COORD y;
+		y.X = 0;
 		for (int i = 0; i <= screen.srWindow.Bottom - 5; i++) {
-			if (view + i > 101) {
+			if (view + i > 105) {
 				break;
 			}
-			std::cout << items[i + view] << characters[i + view];
+			y.Y = i + 2;
+			if (i + view != 1 || i + view != 2 || i + view != 103) {
+				LPCTSTR print = items[i + view] + characters[i + view];
+				WriteConsoleOutputCharacter(console, print, lstrlen(items[i + view]), y, &written);
+				//std::cout << items[i + view] << characters[i + view];
+			}
 		}
 		break;
 	case 4:
@@ -207,9 +223,11 @@ int main() {
 	SetWindow(45, 17);
 	SetWindow(46, 18);
 
-	for (int i = 0; i < 102; i++) {
+	for (int i = 0; i < 105; i++) {
 		characters[i] = 0;
 	}
+
+	std::ios::sync_with_stdio(false);
 
 	while (!GetAsyncKeyState(VK_ESCAPE)) {
 		switch (tab) {
@@ -240,6 +258,7 @@ int main() {
 		}
 
 		waitForUpdate();
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	}
     return 0;
 }
